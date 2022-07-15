919e32584d7c953f7a256a95039d08de5fd20718
Délio <deliocqcosta@gmail.com>
2022-07-15 17:28:06 -0300

    Projeto atualizado com o material da aula do dia 13/07

---
+++ src/models/Images.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "./Users";
+
+export interface IImages {
+  link: string;
+  nome: string;
+  user: Schema.Types.ObjectId | IUser;
+}
+
+const imagesSchema = new Schema<IImages>(
+  {
+    link: {
+      type: Schema.Types.String,
+    },
+    nome: {
+      type: Schema.Types.String,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<IImages>("Images", imagesSchema);
